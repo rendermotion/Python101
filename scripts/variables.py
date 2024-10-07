b4572f27e83f449378f66c418d0bff1d747d5c31
Daniel Ruiz Vargas <daniel@rendermotion.com>
2024-10-06 23:28:59 -0400

    variables

---
+++ scripts/variables.py
@@ -0,0 +1,14 @@
+name = 'daniel'
+the_number = '67'
+my_age = 43
+my_height = 1.73
+
+print(name, my_age, my_height)
+print(type(my_age), type(my_height))
+print(my_age + my_height)
+print(my_age + int(the_number))
+print(str(my_height) + the_number)
+
+
+
+
